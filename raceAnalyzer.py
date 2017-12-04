0e23b4247c2397c0997a2927a1aa43c97cda7707
khangvtran <vinhkhangtran09@gmail.com>
2017-12-04 14:21:10 -0800

    Finising filtering string using RegEx

--- raceAnalyzer.py
+++ raceAnalyzer.py
@@ -3,24 +3,32 @@ import re
 
 
 
-class raceAnalyzer():
+class RaceAnalyzer():
     
     def __init__(self):  
         try:
-            fileName = "lab8input_test.txt"
+            fileName = "lab8input.txt"
             with open(fileName,"r") as infile:
                 header = infile.readline()   # read in the header
                 for line in infile:
                     print(line)
-                    reObj = re.search('(\"[^"]+\").*, (\w{2})\D*(\d+ Mile)',line)
-                    #print(reObj.group(3))
-                    
-                    self.name = reObj.group(1)
-                    self.location = reObj.group(2).upper()   # set the state name as upper case
-                    self.distance = reObj.group(3)
-                    print(self.name)
-                    print(self.location)
-                    print(self.distance)
+                    reObj = re.search('\"([^"]+)\".*, (\w{2})\D*(\d+ Mile).*(Open|Masters)\s+[^\s]+\s+([^\s]+)',line)
+                    if reObj:
+                        self.name = reObj.group(1)
+                        self.location = reObj.group(2).upper()   # set the state name as upper case
+                        self.distance = reObj.group(3)
+                        self.subType = reObj.group(4)
+                        self.raceType= self.distance + " " + self.subType
+                        self.finishTime = reObj.group(5)
+                        print(self.name)
+                        print(self.location)
+                        print(self.distance)
+                        print(self.subType)
+                        print(self.raceType)
+                        print(self.finishTime)
+                    else:
+                        print("############ regex object not found here ############")
+                        
                     
                     
                     
@@ -30,5 +38,5 @@ class raceAnalyzer():
             print(str(e))
             
 
-testObj = raceAnalyzer()
+testObj = RaceAnalyzer()
 testObj
